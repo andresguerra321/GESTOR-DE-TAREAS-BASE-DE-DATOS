2734e74dab22889ddcb8a74da056fb56e871da01
andresguerra321 <andres.felipe.gueco@gmail.com>
2026-08-21 06:53:27 -0500

    feat(main): inject DAO dependencies into TaskManager at application startup

--- src/main/java/com/taskflow/Main.java
+++ src/main/java/com/taskflow/Main.java
@@ -1,5 +1,9 @@
 package com.taskflow;
 
+import com.taskflow.dao.ITaskDAO;
+import com.taskflow.dao.IUserDAO;
+import com.taskflow.dao.TaskDAOImpl;
+import com.taskflow.dao.UserDAOImpl;
 import com.taskflow.ui.MainFrame;
 import javax.swing.SwingUtilities;
 
@@ -9,8 +13,10 @@ import javax.swing.SwingUtilities;
  */
 public class Main {
     public static void main(String[] args) {
-        // Inicializar el gestor de tareas que carga los datos JSON
-        TaskManager taskManager = new TaskManager();
+        // Inicializar el gestor de tareas inyectando las dependencias DAO
+        ITaskDAO taskDAO = new TaskDAOImpl();
+        IUserDAO userDAO = new UserDAOImpl();
+        TaskManager taskManager = new TaskManager(taskDAO, userDAO);
 
         // Lanzar la ventana gráfica principal en el hilo de Swing
         SwingUtilities.invokeLater(() -> {
